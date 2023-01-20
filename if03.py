da3d39afd985af941fabf496a0423f6898c8fceb
bolbekus <bolbekus@gmail.com>
2023-01-20 13:19:37 +0500

    03

--- if03.py
+++ if03.py
@@ -8,4 +8,16 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a > b:
+        if a > c:
+            print(a)
+        else:
+            print(c)
+    else:
+        if b > c:
+            print(b)
+        else: 
+            print(c)
+    return print
+
+print(main(3,7,1))
